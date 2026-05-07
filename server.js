4236d262cf96bdf4d1ed39d441098eea85e4dec0
krizziavallecer2004 <krizziavallecer2004@example.com>
2026-05-07 23:00:45 +0800

    Add Vercel configuration and update PORT handling

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcryptjs");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
